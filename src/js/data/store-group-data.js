975c627099ef4e483726f431ac57e0ea78e90f72
Neal <loseveny@qq.com>
2019-09-06 16:31:39 +1200

    update all the images

--- src/js/data/store-group-data.js
+++ src/js/data/store-group-data.js
@@ -1,29 +1,30 @@
+
 function storyGroupData(path) {
     return {
         storyGroup:
         [
             {
                 thumbnail: {
-                    src: path + '/src/img/mr_logo.png',
+                    src: path + '/src/img/thumbnail_mr_logo_group_1.jpg',
                     title: 'mr_logo'
                 },
                 title: 'story group title1',
                 storyList: [
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image.png',
+                        src: path + '/src/img/story_image_group_1_1.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image2.png',
+                        src: path + '/src/img/story_image_group_1_2.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image.png',
+                        src: path + '/src/img/story_image_group_1_3.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
@@ -31,26 +32,26 @@ function storyGroupData(path) {
             },
             {
                 thumbnail: {
-                    src: path + '/src/img/mr_logo.png',
+                    src: path + '/src/img/thumbnail_mr_logo_group_2.jpg',
                     title: 'mr_logo'
                 },
                 title: 'story group title2',
                 storyList: [
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image2.png',
+                        src: path + '/src/img/story_image_group_2_1.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image.png',
+                        src: path + '/src/img/story_image_group_2_2.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image2.png',
+                        src: path + '/src/img/story_image_group_2_3.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
@@ -58,26 +59,26 @@ function storyGroupData(path) {
             },
             {
                 thumbnail: {
-                    src: path + '/src/img/mr_logo.png',
+                    src: path + '/src/img/thumbnail_mr_logo_group_3.jpg',
                     title: 'mr_logo'
                 },
                 title: 'story group title3',
                 storyList: [
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image.png',
+                        src: path + '/src/img/story_image_group_3_1.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image2.png',
+                        src: path + '/src/img/story_image_group_3_2.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
                     {
                         type: 'image',
-                        src: path + '/src/img/story_image.png',
+                        src: path + '/src/img/story_image_group_3_3.jpg',
                         title: 'image title',
                         link:'https://www.moustacherepublic.com/about-us/'
                     },
@@ -86,5 +87,4 @@ function storyGroupData(path) {
         ]
     };
 }
-
 export default storyGroupData;
